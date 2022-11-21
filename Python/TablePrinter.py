da1f9a770eb49d065bcd0eebc7f456c6ce663451
Lorentsen <104973550+Lorentsen-Cyber@users.noreply.github.com>
2022-11-21 15:10:20 +0100

    Mandag

--- Python/TablePrinter.py
+++ Python/TablePrinter.py
@@ -1,7 +1,17 @@
-
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
 def printTable():
     colWidths = [0] * len(tableData)
+
+    for i in tableData:
+        for j in i:
+            count = -1
+            count += 1
+            if len(j) >= colWidths[count]:
+                colWidths[count] = len(j)
+        
+    print(colWidths)
+
+printTable()
