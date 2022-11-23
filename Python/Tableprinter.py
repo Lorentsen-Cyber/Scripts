191b1e1c20965f767c51d28d23b0fec2f53599d3
Lorentsen <104973550+Lorentsen-Cyber@users.noreply.github.com>
2022-11-23 10:25:20 +0100

    blank

--- Python/Tableprinter.py
+++ Python/Tableprinter.py
@@ -1,8 +1,15 @@
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
-             ['Alice', 'Bob', 'Carol', 'David'],
+             ['Alice', 'Bob', 'Carol', 'David', 'Just Fuck My Shit Up Fam'],
              ['dogs', 'cats', 'moose', 'goose']]
 
 def printTable():
+    longest_list = 0
+    for i in tableData:
+        if len(i) > longest_list:
+            longest_list = len(i)
+    print(longest_list)
+
+    #cal
     colWidths = [0] * len(tableData)
     count = 0
     for i in tableData:
@@ -11,14 +18,11 @@ def printTable():
                 colWidths[count] = len(j)
         count += 1
     count = 0
-    #print(range(tableData[0]))
-    for j in range(1, len(tableData[0])):
-        for i in range(1, len(tableData)):
-            print(tableData[j][i].rjust(colWidths[i]), end=' ')   
-        print()
-
-
-
-    #print(colWidths)
+    
+    #prints the output in correct order
+    for j in range(0, len(tableData[0])):
+        for i in range(0, len(tableData)):
+            #print(tableData[i][j].rjust(colWidths[i]), end=' ')
+            count += 1
 
 printTable()
