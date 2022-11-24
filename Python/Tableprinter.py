08caf52fedc65b0b25b413e2e54c8f35adaa07e6
Lorentsen <104973550+Lorentsen-Cyber@users.noreply.github.com>
2022-11-24 09:29:47 +0100

    k

--- Python/Tableprinter.py
+++ Python/Tableprinter.py
@@ -1,28 +1,35 @@
-tableData = [['apples', 'oranges', 'cherries', 'banana'],
-             ['Alice', 'Bob', 'Carol', 'David', 'Just Fuck My Shit Up Fam'],
-             ['dogs', 'cats', 'moose', 'goose']]
-
+tableData = [['apples', 'oranges', 'cherries', 'banana', 'again'],
+             ['Alice', 'Bob', 'Carol', 'David','Just Fuck My Shit Up Fam', 'twice'],
+             ['dogs', 'cats', 'moose', 'goose', 'thrice']]
 def printTable():
     longest_list = 0
-    for i in tableData:
-        if len(i) > longest_list:
-            longest_list = len(i)
-    print(longest_list)
+    for e in tableData:
+        if len(e) > longest_list:
+            longest_list = len(e)
+    
+    #print(longest_list)
 
     #cal
     colWidths = [0] * len(tableData)
     count = 0
-    for i in tableData:
-        for j in i:
-            if len(j) >= colWidths[count]:
-                colWidths[count] = len(j)
+    for m in tableData:
+        for n in m:
+            if len(n) >= colWidths[count]:
+                colWidths[count] = len(n)
         count += 1
     count = 0
-    
+        
+    for i in tableData:
+        extension = '#' * colWidths[count]
+        for j in range((longest_list - len(i))):
+            i.append(extension)
+        count += 1
+        
+    #print(extension)
     #prints the output in correct order
-    for j in range(0, len(tableData[0])):
+    for j in range(0, longest_list):
         for i in range(0, len(tableData)):
-            #print(tableData[i][j].rjust(colWidths[i]), end=' ')
-            count += 1
+            print(tableData[i][j].rjust(colWidths[i]), end=' ')
+        print('')
 
 printTable()
