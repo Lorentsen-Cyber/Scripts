8cbca3c70b255b71d7152b1cf69c465ce9df09ca
Lorentsen <104973550+Lorentsen-Cyber@users.noreply.github.com>
2023-03-07 08:32:40 +0100

    update

---
+++ Python/forfun.py
@@ -0,0 +1,11 @@
+fruits =['Mango' ,'Apple','Orange']
+
+for i in range(len(fruits)):
+    print(i, fruits[i])
+
+print()
+
+for index, fruit in enumerate(fruits):
+    print(index, fruit)
+
+enumerate(fruits)
--- Python/tempCodeRunnerFile.py
+++ Python/tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-model
+print
